1aa87762dd39d03f26d35089c58335c95883bdd4
ParkSeongtaek <56757197+ParkSeongtaek@users.noreply.github.com>
2021-07-15 16:39:47 +0900

    박성택

---
+++ Assets/Script/ControlColor.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ControlColor : MonoBehaviour
+{
+    GameObject Image;
+    GameObject CurrentTouch;
+    Color Color;
+    bool getcolor;
+
+    private void Start()
+    {
+        Image = GameObject.Find("Canvas").transform.Find("NowColor").gameObject;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Physics.Raycast(ray, out hit);
+
+            if (hit.collider != null)
+            {
+                CurrentTouch = hit.transform.gameObject;
+                if (!GameManager.Instance.saveColor)
+                {
+                    CurrentTouch.GetComponent<Renderer>().material.color = GameManager.Instance.Getcolor();
+                    GameManager.Instance.saveColor = true;
+                    Debug.Log("True");
+                }
+                else
+                {
+                    GameManager.Instance.Setcolor(CurrentTouch.GetComponent<Renderer>().material.color);
+                    Image.GetComponent<Image>().color = GameManager.Instance.Getcolor();
+                    GameManager.Instance.saveColor = false;
+                    Debug.Log("False");
+
+                }
+
+            }
+        }
+
+
+
+    }
+}
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    //string NowColor;
+    public bool saveColor;
+    Color Color;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Instance = this;
+        Instance.saveColor = true;
+
+    }
+
+    void Start()
+    {
+        //getColor = true;
+        //Instance.NowColor = GameObject.Find("Canvas").transform.GetChild(0).gameObject.name;
+        //Debug.Log(Instance.NowColor);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   // public string GetNowColorName()
+    //{
+    //    return NowColor;
+    //}
+    public void Setcolor(Color color)
+    {
+        Instance.Color = color;
+    }
+    public Color Getcolor()
+    {
+        return Instance.Color;
+    }
+
+
+}
