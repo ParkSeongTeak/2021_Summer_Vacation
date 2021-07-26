2a34cf82468c7fa5a2e07dfa236572c5de29a074
dynene <ekdlsb02@gmail.com>
2021-07-27 04:13:34 +0900

    Update ObjControl.cs
    SearchNear 함수 수정

--- Assets/Script/Control/ObjControl.cs
+++ Assets/Script/Control/ObjControl.cs
@@ -5,33 +5,15 @@ using UnityEngine.UI;
 
 public class ObjControl : MonoBehaviour
 {
-
-
-
     List<GameObject> neighborObjList = new List<GameObject>();
 
     private void Start()
     {
         Invoke("GetNeighbor", 0.2f);
-           
-       
+
+
     }
-    //Collion
 
-    //전체 gamemanager에 bool processing 변수 추가
-    //어떤 obj에 클릭 발생하여 인접 obj 검색 및 색 변경 작업 중인지 여부를 나타냄
-
-    //클릭 이벤트 발생 시 모든 obj의 processed를 0으로 초기화!!! ..?
-
-    ///////////////////////////////////////////
-
-    //이 obj의 인접 obj에 대해 search 실행했는지 여부
-    //이 obj의 인접 obj가 search를 실행하면서 true로 변경해줌
-    bool processed = false;
-
-    
-
-    
     public void Search(GameObject gameObject, Color originalColor)
     {
 
@@ -65,35 +47,35 @@ public class ObjControl : MonoBehaviour
         {
             this.GetComponent<Renderer>().material.color = gameObject.GetComponent<Renderer>().material.color;// 색 바꾸기
             //Debug.Log("Hello");
-            for (int i = 0 ; i < neighborObjList.Count; i++)
+            for (int i = 0; i < neighborObjList.Count; i++)
             {
-                neighborObjList[i].GetComponent<ObjControl>().Search(this.gameObject,originalColor);        //인접 큐브에 전파;
-               // Debug.Log("Hello");
+                neighborObjList[i].GetComponent<ObjControl>().Search(this.gameObject, originalColor);        //인접 큐브에 전파;
+                // Debug.Log("Hello");
             }
 
 
         }
-
-        //processed = true;
-
-
-
-
-        //gameObject.GetComponent<ObjControl>().processed = true;
-        // + (추가해야 할 것)
-        //현재 search를 실행중인 obj(this)를 클릭했을 때의 event를 호출 ..?  //  그 
-        //(this의 인접 obj에 대해 Search()를 호출하는 효과)
     }
 
     //////////////////////////////////////////
 
-  
+
 
     void SearchNear(Queue<GameObject> gameObjects, GameObject Start)
     //SearchNear 함수는 queue에 담긴 Obj를 top부터 확인하며 최초로 start와 다른 색을 지닌 Obj를 Search함수로 보내는 함수입니다 잔여 queue는 clear해줍니다.
     //아마? gameObjects[0] ==start일 것 같긴 한데 버그 방지 (당연히 gameObjects[0] 는 이해를 위한 표현입니다)
     {
-
+        GameObject tmp;
+        while (gameObjects.Count != 0)
+        {
+            tmp = gameObjects.Dequeue();
+            if (tmp.GetComponent<Renderer>().material.color != Start.GetComponent<Renderer>().material.color)
+            {
+                tmp.GetComponent<ObjControl>().Search(Start, Start.GetComponent<Renderer>().material.color);
+                gameObjects.Clear();
+                break;
+            }
+        }
     }
 
     void GetNeighbor()
@@ -108,11 +90,10 @@ public class ObjControl : MonoBehaviour
 
         for (int i = 0; i < neighborObjList.Count; i++)
         {
-            Debug.Log(" In Obj Con "+neighborObjList[i].name);
+            Debug.Log(" In Obj Con " + neighborObjList[i].name);
         }
 
     }
 
 }
 
-  
