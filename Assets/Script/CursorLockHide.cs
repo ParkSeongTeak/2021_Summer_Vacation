082d4f1343aab338fdcf2d89a0bd13dbccee5c89
hyuk-kwak <starcaft99@nate.com>
2021-07-31 22:56:45 +0900

    Dragbox&CursorLocked

---
+++ Assets/Script/CursorLockHide.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorLockHide : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //W키를 누르면 마우스 커서를 게임 중앙 좌표에 고정 시키고 마우스 커서가 보임
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            // 마우스 커서 보이게 함
+            Cursor.visible = true;
+        }
+        //Q키를 누르면 마우스 커서를 게임 중앙 좌표에 고정시키고 마우스 커서 안보임
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            //마우스 커서 보이지 않게함
+            Cursor.visible = false;
+        }
+
+    }
+}
---
+++ Assets/Script/Mouse_DragRendering.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse_DragRendering : MonoBehaviour
+{
+    public Vector3 startPos;
+    public Vector3 endPos;
+    public GameObject tempDrag;
+    public GameObject DragBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //마우스 버튼을 눌렸을 때
+        if (Input.GetMouseButtonDown(0))
+        {
+            //마우스 시작 위치 저장
+            startPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
+
+            //시작위치에 프리팩 설정 
+            tempDrag = Instantiate(DragBox, startPos, Quaternion.identity) as GameObject;
+            //시작 위치 출력
+            Debug.Log("mouse:" + startPos.x + " , " + startPos.y);
+        }
+
+        //마우스 버툰이 눌러진 상태일 때
+        if (Input.GetMouseButton(0))
+        {
+            //현재 마우스의 위치
+            endPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
+
+            //드래그 이미지의 위치를 마우스 위치로
+            tempDrag.transform.localScale = new Vector3(endPos.x - startPos.x, startPos.y - endPos.y, endPos.z);
+
+            //이부분을 수정해야 될거 같은데 Scale 가지고 어떻게 해야될지 감이 안와요 ;
+
+            Debug.Log("mouseDown");
+        }
+
+        //마우스 버튼을 땠을 때
+        if (Input.GetMouseButtonUp(0))
+        {
+            Destroy(tempDrag);
+            Debug.Log("mouseUp");
+        }
+    }
+}
