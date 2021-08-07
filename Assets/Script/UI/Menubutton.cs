6bb72003c7cbc3106ac3beb9a1837ec49bd4334e
ParkSeongtaek <56757197+ParkSeongtaek@users.noreply.github.com>
2021-08-07 09:28:55 +0900

    박성택 데스크톱

---
+++ Assets/Script/UI/Menubutton.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menubutton : MonoBehaviour
+{
+    public GameObject[] SlideImg = new GameObject[4];
+    public GameObject[] StartImg = new GameObject[2];
+
+    GameObject Target = null;
+    //public Transform target;
+    public float smoothTime = 0.3F;
+    private Vector3 velocity = Vector3.zero;
+    Vector3 targetPosition = new Vector3(-543,0,0);
+    int idx;
+
+    void Update()
+    {
+        if (Target != null)
+        {
+            // Define a target position above and behind the target transform
+            //Vector3 targetPosition = Target.transform.TransformPoint(new Vector3(0, 5, -10));
+
+            // Smoothly move the camera towards that target position
+            Target.transform.position = Vector3.SmoothDamp(SlideImg[idx].transform.position, targetPosition, ref velocity, smoothTime);
+        }
+
+
+
+    }
+
+    void StartButton()
+    {
+        Invoke("LoadGame", 1.5f);
+        idx = 0;
+        Target = SlideImg[idx];
+
+        
+    }
+    
+
+    void LoadGame()
+    {
+        SceneManager.LoadScene("Stage_1");
+    }
+  
+}
---
+++ Assets/Script/UI/Opacity.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glow : MonoBehaviour
+{
+    public float activetime = 0.3f;
+    const float fixedTime = 3.0f;
+    float timer = 0.0f;
+    float glowtimer = 0.0f;
+    bool up = false;
+    SpriteRenderer spr;
+    Color color;
+
+
+
+    void Start()
+    {
+        spr = this.gameObject.GetComponent<SpriteRenderer>();
+
+        this.color = spr.color;
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+
+        this.timer += Time.deltaTime;
+        if (timer > activetime)
+        {
+            if (this.glowtimer >= 0.5f)
+            {
+                this.glowtimer = 0.0f;
+                this.up = !this.up;
+
+            }
+
+            if (this.up)
+            {
+
+                if (this.color.a > 0)
+                {
+                    this.color.a -= 0.03f;
+                    spr.color = this.color;
+
+                    Debug.Log("D" + spr.color.a);
+                    //this.gameObject.GetComponent<SpriteRenderer>().color = color;
+                }
+            }
+            else
+            {
+                if (this.color.a < 1)
+                {
+                    this.color.a += 0.03f;
+                    spr.color = this.color;
+                    Debug.Log("Up" + spr.color.a);
+
+                    //this.gameObject.GetComponent<SpriteRenderer>().color = color;
+                }
+
+            }
+
+            if (this.timer > 3.0f)
+            {
+                Destroy(this.gameObject);
+            }
+
+        }
+    }
+}
