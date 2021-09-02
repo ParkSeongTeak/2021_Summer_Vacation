b4d61fd09f6bb48ae1ea19e2834af9e88b2104bc
dynene <ekdlsb02@gmail.com>
2021-09-02 21:21:19 +0900

    MainScene

---
+++ Assets/Script/UI/MainBtn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MainBtn : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    GameObject btn;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        eventData.pointerEnter.transform.Translate(new Vector3(50, 0, 0));
+        btn = eventData.pointerEnter;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        btn.transform.Translate(new Vector3(-50, 0, 0));
+    }
+}
