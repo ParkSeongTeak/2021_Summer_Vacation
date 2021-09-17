0a2e4e794396260be2192742cc10c4bb07bbe753
totoonion <totoonion@naver.com>
2021-09-17 15:09:00 +0900

    벽 색깔 버그 수정안
    메터리얼 초기설정을 흰색으로 해서 구운다음에 게임 시작 후 색을 바꾸는 방식

---
+++ Assets/Script/MaterialChangeJH.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChangeJH : MonoBehaviour
+{
+    [SerializeField]
+    Material ColorRendererRed;
+    [SerializeField]
+    Material ColorRendererGreen;
+    [SerializeField]
+    Material ColorRendererBlue;
+    [SerializeField]
+    Material ColorRendererOrange;
+    [SerializeField]
+    Material ColorRendererBlack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ColorRendererRed.color = new Color(249f/255f, 66f/255f, 58f/255f, 1f);
+        ColorRendererGreen.color = new Color(133f/255f, 245f/255f, 168f/255f, 1f);
+        ColorRendererBlue.color = new Color(128f/255f, 188f/255f, 251f/255f, 1f);
+    }
+
+    void OnApplicationQuit() {
+        ColorRendererRed.color = new Color(240f/255f, 240f/255f, 240f/255f, 1f);
+        ColorRendererGreen.color = new Color(240f/255f, 240f/255f, 240f/255f, 1f);
+        ColorRendererBlue.color = new Color(240f/255f, 240f/255f, 240f/255f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
